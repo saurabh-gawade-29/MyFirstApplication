59a9527eb4dd589ec591d3a8cdbe1d571cd67354
Saurabh Gawade <saurabhgawade2920@gmail.com>
2025-08-16 20:03:22 +0530

    form created

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -1,24 +1,74 @@
 package com.example.myfirstapplication;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.*;
+import android.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextField, editTextArea;
+    RadioGroup radioGroup;
+    Spinner spinnerCountry;
+    CheckBox checkSubscribe;
+    ImageButton imageButton;
+    Button buttonSubmit;
+    String selectedImageButton = "Not pressed";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        editTextField = findViewById(R.id.editTextField);
+        editTextArea = findViewById(R.id.editTextArea);
+        radioGroup = findViewById(R.id.radioGroup);
+        spinnerCountry = findViewById(R.id.spinnerCountry);
+        checkSubscribe = findViewById(R.id.checkSubscribe);
+        imageButton = findViewById(R.id.imageButton);
+        buttonSubmit = findViewById(R.id.buttonSubmit);
+
+        String[] countries = {"India", "USA", "UK"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, countries);
+        spinnerCountry.setAdapter(adapter);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedImageButton = "Camera Image Button was pressed";
+                Toast.makeText(MainActivity.this, "ImageButton pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String textField = editTextField.getText().toString();
+                String textArea = editTextArea.getText().toString();
+
+                int selectedRadioId = radioGroup.getCheckedRadioButtonId();
+                RadioButton selectedRadioButton = findViewById(selectedRadioId);
+                String gender = selectedRadioButton != null ? selectedRadioButton.getText().toString() : "None";
+
+                String country = spinnerCountry.getSelectedItem().toString();
+                String subscribe = checkSubscribe.isChecked() ? "Yes" : "No";
+
+                String message = "Text Field: " + textField +
+                        "\nText Area: " + textArea +
+                        "\nGender: " + gender +
+                        "\nCountry: " + country +
+                        "\nSubscribed: " + subscribe +
+                        "\nImageButton status: " + selectedImageButton;
+
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Form Details")
+                        .setMessage(message)
+                        .setPositiveButton("OK", null)
+                        .show();
+            }
         });
     }
 }
+
