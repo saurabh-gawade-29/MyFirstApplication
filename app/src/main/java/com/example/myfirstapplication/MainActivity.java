e4812503d51523ba77108628676dd642120d71a7
Saurabh Gawade <saurabhgawade2920@gmail.com>
2025-10-06 00:53:01 +0530

    Animation added

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -1,24 +1,30 @@
 package com.example.myfirstapplication;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button btnAnimate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        imageView = findViewById(R.id.imageView);
+        btnAnimate = findViewById(R.id.btnAnimate);
+
+        // Load animation from XML
+        Animation moveAnim = AnimationUtils.loadAnimation(this, R.anim.move);
+
+        btnAnimate.setOnClickListener(v -> {
+            imageView.startAnimation(moveAnim);
         });
     }
 }
